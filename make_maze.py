771db5c1e1a0db3688ad47a68ddeeceb8983f8dd
Frixos Sotiroudis <fri3os_21@hotmail.com>
2015-03-28 21:10:37 +0200

    savvato

--- make_maze.py
+++ make_maze.py
@@ -4,8 +4,10 @@ import random
 n = int(sys.argv[1])
 start_x = int(sys.argv[2])
 start_y = int(sys.argv[3])
-seed = sys.argv[4]
+input_seed = sys.argv[4]
 output_file = sys.argv[5]
+random.seed(input_seed)
+
 
 graph = {}
 
@@ -14,46 +16,81 @@ for i in range(0, n*n):
     if i==0 :
         neighbours.append(i+1)
         neighbours.append(i+n)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
     elif i==n-1:
         neighbours.append(i-1)
         neighbours.append(i+n)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
     elif i==(n-1)*n:
         neighbours.append(i+1)
         neighbours.append(i-n)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
     elif i==n*n-1 :
         neighbours.append(i-1)
         neighbours.append(i-n)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
     elif i<n:
         neighbours.append(i-1)
         neighbours.append(i+1)
         neighbours.append(i+n)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
     elif i%n==0: 
         neighbours.append(i-n)
         neighbours.append(i+n)
         neighbours.append(i+1)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
     elif (i+1)%n==0:
         neighbours.append(i-n)
         neighbours.append(i+n)
         neighbours.append(i-1)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
     elif i>(n*n)-n:
         neighbours.append(i-1)
         neighbours.append(i+1)
         neighbours.append(i-n)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
     else:
         neighbours.append(i-1)
         neighbours.append(i+1)
         neighbours.append(i+n)
         neighbours.append(i-n)
-        graph[i] = neighbours
+        graph[i] = random.sample(neighbours, len(neighbours))
    
 print(graph)
 
 
+start= start_x*n + start_y       
+
+print (start)
+def recursive_dfs(graph, start, path=[]):
+  path=path+[start]
+  for node in graph[start]:    
+    if not node in path:
+      path=recursive_dfs(graph, node, path)
+  #print (path)
+  return path
+
+
+
+path=recursive_dfs(graph, start, path=[])
+def filec(path):
+    file = open("newfile.txt", "w")
+    for i in range (0,n*n-1):
+        x=path[i]/n
+        y=path[i]%n
+        x2=path[i+1]/n
+        y2=path[i+1]%n
+        file.write('(')
+        file.write(str(x))
+        file.write(',')
+        file.write(str(y))
+        file.write(')')
+        file.write(', ')
+        file.write('(')
+        file.write(str(x2))
+        file.write(',')
+        file.write(str(y2))
+        file.write(')')
+        file.write('\n')
+    file.close()
+filec(path)
